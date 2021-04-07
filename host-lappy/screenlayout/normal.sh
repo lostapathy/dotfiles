2c6eaf3d675034a5fb756f20557cad19c5add15e
Joe Francis <joe@lostapathy.com>
2021-04-07 15:33:39 -0500

    screen profiles for laptop

--- host-lappy/screenlayout/normal.sh
+++ host-lappy/screenlayout/normal.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 3200x1800 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-
+xrandr --output VIRTUAL1 --off --output eDP-1 --primary --mode 3200x1800 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off --output DP-1 --off
