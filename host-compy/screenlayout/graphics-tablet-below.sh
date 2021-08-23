1eb42be0347438376a02a10d791ab4cb6a7717a5
Joe Francis <joe@lostapathy.com>
2021-08-22 20:16:45 -0500

    latest desktop updates

--- host-compy/screenlayout/graphics-tablet-below.sh
+++ host-compy/screenlayout/graphics-tablet-below.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 960x2160 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --off --output HDMI-A-0 --mode 1920x1080 --pos 960x2160 --rotate normal --output DVI-D-0 --off
 
 # This maps the pen output to the drawing tablet.
 # The 25 is the id from `xinput`
+sleep 2 # Need to let the input "settle" before monkeying with the input or else this may not stick
 STYLUS_INPUT=`xinput | grep stylus | awk '{print $6}' | awk -F= '{print $2}'`
 xinput map-to-output $STYLUS_INPUT HDMI-A-0
 
 i3-msg workspace "3: GIMP", move workspace to output HDMI-A-0
+i3-msg workspace "7: xournal", move workspace to output HDMI-A-0
 
 # dump out buttons with xsetwacom -s --get 22 all
 WACOM_PAD=`xsetwacom --list devices |grep "Pad pad" |awk '{print $6}'`
